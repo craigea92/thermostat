987269d1df160f687bd21ce1c2cdbcd4cfb3702c
Craig Eaton <craigea92@gmail.com>
2021-07-07 15:53:04 +0100

    13 Passing Tests

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -5,7 +5,10 @@ class Thermostat{
     this.MINIMUM_TEMPERATURE = 10;
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
-    this.temperature = 20;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.powerSavingMode = true;
   }
   getCurrentTemperature() {
@@ -40,5 +43,17 @@ class Thermostat{
   }
   switchPowerSavingModeOn() {
     this.powerSavingMode = true;
+  }
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE
+  }
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   }
 };
